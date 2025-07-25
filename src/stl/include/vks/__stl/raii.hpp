a1c107b47509bcda7b9bf954a8e13240e9405117
SnowinterCat <SnowinterCat@github.com>
2025-07-25 16:26:52 +0800

    [rename]stl

--- src/stl/include/vks/__stl/raii.hpp
+++ src/stl/include/vks/__stl/raii.hpp
@@ -1,6 +1,6 @@
 #pragma once
 #include <vks/config.hpp>
-#include <vks/__stl/stl_config.hpp>
+#include <vks/__stl/__config.hpp>
 // Standard Library
 #include <cstdio>
 #include <memory>
--- src/stl/include/vks/stl.hpp
+++ src/stl/include/vks/stl.hpp
@@ -1,5 +1,5 @@
 #pragma once
 #include <vks/config.hpp>
 
-#include <vks/__stl/stl_config.hpp>
+#include <vks/__stl/__config.hpp>
 #include <vks/__stl/raii.hpp>
