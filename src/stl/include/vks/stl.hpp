f45886dad266f1fb1a223d62c4598b502d6116c8
SnowinterCat <SnowinterCat@github.com>
2025-07-25 16:03:34 +0800

    [update]stl

---
+++ src/stl/include/vks/stl.hpp
@@ -0,0 +1,2 @@
+#pragma once
+#include <vks/config.hpp>
--- src/stl/xmake.lua
+++ src/stl/xmake.lua
@@ -1,28 +1,22 @@
-
-
 target("stl")
     set_kind("$(kind)")
     set_targetdir("$(libdir)")
 
-    on_load(function (target)
-        function Camel(str)
-            return str:sub(1, 1):upper() .. str:sub(2)
-        end
-        import("core.project.project")
-        target:set("basename", "Vks" .. Camel(target:name()))
-    end)
+    add_deps("config")
+    -- add_packages("spdlog")
 
-    add_deps("basic")
-    add_rules("targetclean")
-    add_defines("VKS_STL_EXPORTS")
-
-    -- version
-    -- set_configdir("./")
-    -- add_configfiles("*.rc.in")
-    -- add_files("*.rc")
-    -- target files
     add_includedirs("include", {public = true})
     add_headerfiles("include/(**)")
-    -- add_headerfiles("src/**.hpp", "src/**.h", {install = false})
+    -- add_headerfiles("src/(**.hpp)", {install = false})
     add_files("src/**.cpp")
+
+    on_load(function (target) 
+        import("lua.auto", {rootdir = os.projectdir()})
+        auto().target_autoname(target)
+        auto().library_autodefine(target)
+    end)
+    after_build(function (target) 
+        import("lua.auto", {rootdir = os.projectdir()})
+        auto().target_autoclean(target)
+    end)
 target_end()
